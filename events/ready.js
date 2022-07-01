d9e68fe6027adef35a9fa86140f51affa8a27df7
BizarreAvatar <andrewtamayo6@gmail.com>
2022-06-30 22:54:56 -0500

    a

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 const Mongo = require('../utils/Mongo');
+const WOKCommands = require('wokcommands');
+const path = require('path');
 
 module.exports = {
     once: true,
@@ -8,5 +10,9 @@ module.exports = {
     
         console.log(`Logged in as ${client.user.tag}`);
         Mongo.init();
+
+        new WOKCommands(client, {
+            commandsDir: path.resolve('', 'commands')
+        })
     }
 }
