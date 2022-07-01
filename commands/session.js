b3767e56404c26999353e80c553929892403e00a
BizarreAvatar <andrewtamayo6@gmail.com>
2022-06-30 23:18:35 -0500

    a

--- commands/session.js
+++ commands/session.js
@@ -16,19 +16,18 @@ module.exports = {
         const validSubcommands = ['help', 'start', 'settings'];
         const [subcommand] = args;
 
-        if (subcommand.toLowerCase() == "help" || !validSubcommands.contains(subcommand)) {
+        if (subcommand.toLowerCase() == "help" || !validSubcommands.includes(subcommand)) {
             const helpEmbed = new MessageEmbed()
                 .setColor(primary_color)
-                .setTitle('Sessions | Help Embed')
-                .setDescription('This is the help embed for the sessions subcommands! To utilize these, type /session <subcommand>')
+                .setTitle('ℹ️ Help Embed ℹ️')
+                .setDescription('This is the help embed for the sessions subcommands!')
                 .addFields(
-                    {name: 'start', value: "Starts a session by dming you an embed to show you profiles to swipe on!"},
-                    {name: 'settings', value: "Allows you to adjust what profiles you see by things such as location, games played, and age."},
+                    {name: '♦️ /session start', value: "Starts a session by dming you an embed to show you profiles to swipe on!"},
+                    {name: '♦️ /session settings <option> <value>', value: "Allows you to adjust what profiles you see by things such as location, games played, and age."},
 
                 )
             interaction.reply({embeds: [helpEmbed], content: `<@${interaction.user.id}>`})
         } 
         
-        interaction.reply('Test!');
     },
 }
