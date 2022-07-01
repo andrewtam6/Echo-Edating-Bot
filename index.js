f2f0533e6457dac998f114ce714f6c339ff58186
BizarreAvatar <andrewtamayo6@gmail.com>
2022-06-30 22:13:56 -0500

    a

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: new Discord.Intents(32767) });
 
 require('dotenv').config();
+require(`./handlers/event_handler`)(client, Discord)
 
 client.commands = new Discord.Collection();
-
-['command_handler', 'event_handler'].forEach(handler => {
-    require(`./handlers/${handler}`)(client, Discord)
-})
 
 client.login(process.env.TOKEN);
