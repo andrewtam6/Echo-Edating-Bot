dbdee8ac499935a00848975fbeaed468a002123e
BizarreAvatar <andrewtamayo6@gmail.com>
2022-07-01 12:07:26 -0500

    a

--- utils/classes/Profile.js
+++ utils/classes/Profile.js
@@ -1,7 +1,15 @@
+const profileSchema = require("../../schemas/profileSchema");
+
 const ProfileClass = {};
 
 ProfileClass.create = (email, DOB, imageURL, Bio) => {
-    
+    // Saves DB Data
+    new profileSchema({
+        email: email, 
+        DOB: DOB,
+        mainProfileImage: imageURL,
+        Bio: Bio,
+    }).save();
 }
 
 module.exports = ProfileClass;
