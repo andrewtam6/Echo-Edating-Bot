8aad66600a37e7dd5984783c5efa0c4b46cb5243
BizarreAvatar <andrewtamayo6@gmail.com>
2022-07-03 21:19:49 -0500

    a

--- utils/util.js
+++ utils/util.js
@@ -22,8 +22,7 @@ module.exports = {
     },
     format: (type, value) => {
         if (type.toLowerCase() === 'gender') {
-            const firstLetter = value.slice(0, 1);
-            return `${firstLetter.toUpperCase()}${value.slice(1).toLowerCase()}`;
+            return value.toLowerCase();
         } 
     }
 }
